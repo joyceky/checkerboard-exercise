e3f32f47856be23d37ab6c7033bdd30804fa4dac
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-23 18:00:25 -0600

    checkerboard complete, try other exercises

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+"use strict";
 // Your JS goes here
+$( document ).ready(function() {
+  createContainer();
+});
+
+var squareHeight = "11.1%";
+
+var blackSquare = {
+    "background-color": "black",
+    "width": "11.1%",
+    "float": "left",
+    "padding-bottom": "11.1%",
+    "height": squareHeight
+  };
+
+var redSquare = {
+    "background-color": "red",
+    "width": "11.1%",
+    "float": "left",
+    "padding-bottom": "11.1%",
+    "height": squareHeight
+  };
+
+function createContainer(){
+
+  $("body").html("<div id=\"container\"></div>");
+  $("#container").css({
+          "height": "100%",
+          "width": "100%"
+  });
+  buildCheckerboard();
+}
+//
+
+function buildCheckerboard() {
+  var container = $("#container");
+  for (var i = 0; i <=48; i++) {
+    $("<div></div>").css(blackSquare).appendTo(container);
+    $("<div></div>").css(redSquare).appendTo(container);
+ }
+ $("<div></div>").css(blackSquare).appendTo(container);
+}
