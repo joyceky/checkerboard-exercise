bc912228715a92f78788a5197a38eb919bbe37ee
Kyle Joyce <kyyjoyce@gmail.com>
2016-10-23 18:41:14 -0600

    multicolor board working

--- script.js
+++ script.js
@@ -2,42 +2,68 @@
 // Your JS goes here
 $( document ).ready(function() {
   createContainer();
+  // buildCheckerboard();
+  // buildMulticolorBoard();
+  buildFlashingBoard();
 });
 
-var squareHeight = "11.1%";
-
-var blackSquare = {
-    "background-color": "black",
-    "width": "11.1%",
-    "float": "left",
-    "padding-bottom": "11.1%",
-    "height": squareHeight
-  };
-
-var redSquare = {
-    "background-color": "red",
-    "width": "11.1%",
-    "float": "left",
-    "padding-bottom": "11.1%",
-    "height": squareHeight
-  };
-
 function createContainer(){
-
   $("body").html("<div id=\"container\"></div>");
   $("#container").css({
           "height": "100%",
           "width": "100%"
   });
-  buildCheckerboard();
 }
-//
 
 function buildCheckerboard() {
+  var blackSquare = {
+      "background-color": "black",
+      "width": "11.1%",
+      "float": "left",
+      "padding-bottom": "11.1%",
+      "height": "11.1%"
+    };
+
+  var redSquare = {
+      "background-color": "red",
+      "width": "11.1%",
+      "float": "left",
+      "padding-bottom": "11.1%",
+      "height": "11.1%"
+    };
+
   var container = $("#container");
   for (var i = 0; i <=48; i++) {
     $("<div></div>").css(blackSquare).appendTo(container);
     $("<div></div>").css(redSquare).appendTo(container);
  }
  $("<div></div>").css(blackSquare).appendTo(container);
+}
+
+/**************************************************************************/
+
+function buildMulticolorBoard() {
+  var container = $("#container");
+
+  var multiSquare = {
+      "width": "11.1%",
+      "float": "left",
+      "padding-bottom": "11.1%",
+      "height": "11.1%"
+    };
+
+  for (var i = 0; i <=97; i++) {
+    var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var square = $("<div></div>").css(multiSquare);
+    square.css("background-color", color).appendTo(container);
+ }
+ $("<div></div>").css(multiSquare).appendTo(container);
+ square.css("background-color", color).appendTo(container);
+}
+
+/**************************************************************************/
+
+
+function buildGradientBoard() {
+
 }
